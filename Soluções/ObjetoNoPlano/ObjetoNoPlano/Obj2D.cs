e2ed7cc35c52a7f90ebfc5c8cf3289b616983b47
João Macedo <joaooliveiramacedo422@gmail.com>
2017-06-06 15:01:28 -0300

    Objeto no PLano

---
+++ Soluções/ObjetoNoPlano/ObjetoNoPlano/Obj2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetoNoPlano
+{
+    class Obj2D
+    {
+         public int x, y;
+
+        public void AndDir() {
+            x++;
+        }
+
+        public void AndEsq() {
+            x--;
+        }
+
+        public void Subir() {
+            y++;
+        }
+
+        public void Descer() {
+            y--;
+    }
+}
